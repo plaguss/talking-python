d8e9f46fa219ddb5505b119b6ab1c5d34485faf5
agustin <agustin.piqueres@gmail.com>
2023-05-01 19:56:19 +0200

    refactor: change layout of the program for simplicity

--- talking_python/clean_transcripts.py
+++ talking_python/clean_transcripts.py
@@ -28,13 +28,45 @@ import spacy
 from prefect import flow, task
 from prefect.logging import get_logger
 
-import talking_python.settings as setts
+from dotenv import dotenv_values
+from pydantic import BaseSettings
 
 nlp = spacy.load("en_core_web_sm")
 
 log = get_logger()
 
-settings = setts.Settings()
+config = dotenv_values(".env")
+
+
+class Settings(BaseSettings):
+    # _root: Path = Path(__file__).parent.parent.parent
+    _root: Path = Path(__file__).parent.parent
+    transcripts_folder: Path = _root / "talk-python-transcripts/transcripts"
+    flow_results: Path = _root / "flow_results"
+    transcript_filenames: Path = flow_results / "transcript_filenames.txt"
+    cleaned_transcripts: Path = flow_results / "cleaned_transcripts"
+    file_lengths: Path = flow_results / "file_lengths.json"
+    flow_environ_local: bool = True if config.get("FLOW_ENVIRON") == "local" else False
+
+
+settings = Settings()
+
+if not settings.flow_results.is_dir():
+    settings.flow_results.mkdir()
+
+
+if not settings.cleaned_transcripts.is_dir():
+    settings.cleaned_transcripts.mkdir()
+
+
+def transcript_filenames() -> list[Path]:
+    """Get the filenames with the original transcripts."""
+    with settings.transcript_filenames.open() as f:
+        filenames = [settings.transcripts_folder / f for f in f.read().splitlines()]
+    return filenames
+
+
+# settings = setts.Settings()
 
 
 if settings.flow_environ_local:
