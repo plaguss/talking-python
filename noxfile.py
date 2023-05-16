f86ab711f5c05a67b0241f39761454452eac2849
agustin <agustin.piqueres@gmail.com>
2023-05-16 20:39:51 +0200

    chore: add functions to iterate over the internal package

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,26 @@ import sys
 
 PYTHON = sys.executable
 
+PACKAGE_NAME = "src/talking_python"
+
+
+@nox.session
 def compile_dependencies(session):
     session.run(PYTHON, "-m", "piptools", "compile", "requirements/dev.in")
     session.run(PYTHON, "-m", "piptools", "compile", "requirements/dev.in")
 
+
+@nox.session
+def format_package(session):
+    session.run("black", PACKAGE_NAME)
+    session.run("ruff", PACKAGE_NAME, "--fix")
+
+
+@nox.session
+def build_package(session):
+    session.run(PYTHON, "-m", "build")
+
+
+@nox.session
+def install_package(session):
+    session.run(PYTHON, "-m", "pip", "install", ".", "-w")
