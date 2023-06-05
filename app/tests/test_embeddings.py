4d50075e1bbd035ca3a196c856dac008c6d50105
agustin <agustin.piqueres@gmail.com>
2023-06-05 22:41:58 +0200

    test: add test for grouping functions

--- app/tests/test_embeddings.py
+++ app/tests/test_embeddings.py
@@ -8,31 +8,31 @@ import numpy as np
 def client_query_result():
     yield {
         "metadatas": [
-                {"title": "402-polars.txt", "line": "209"},
-                {"title": "012.txt", "line": "198"},
-                {"title": "012.txt", "line": "197"},
-                {
-                    "title": "410-intersection-of-tabular-data-and-general-ai.txt",
-                    "line": "113",
-                },
-                {"title": "341-25-pandas-functions.txt", "line": "55"},
-                {"title": "414-startup-row.txt", "line": "70"},
-                {"title": "402-polars.txt", "line": "210"},
-                {"title": "338-cibuildwheel-scikit-hep.txt", "line": "240"},
-                {"title": "402-polars.txt", "line": "218"},
-                {"title": "402-polars.txt", "line": "221"},
+            {"title": "402-polars.txt", "line": "209"},
+            {"title": "012.txt", "line": "198"},
+            {"title": "012.txt", "line": "197"},
+            {
+                "title": "410-intersection-of-tabular-data-and-general-ai.txt",
+                "line": "113",
+            },
+            {"title": "341-25-pandas-functions.txt", "line": "55"},
+            {"title": "414-startup-row.txt", "line": "70"},
+            {"title": "402-polars.txt", "line": "210"},
+            {"title": "338-cibuildwheel-scikit-hep.txt", "line": "240"},
+            {"title": "402-polars.txt", "line": "218"},
+            {"title": "402-polars.txt", "line": "221"},
         ],
         "distances": [
-                0.3780611753463745,
-                0.40761131048202515,
-                0.42179733514785767,
-                0.4531496465206146,
-                0.4663689434528351,
-                0.47604429721832275,
-                0.485452264547348,
-                0.5037158727645874,
-                0.5327827334403992,
-                0.5412342548370361,
+            0.3780611753463745,
+            0.40761131048202515,
+            0.42179733514785767,
+            0.4531496465206146,
+            0.4663689434528351,
+            0.47604429721832275,
+            0.485452264547348,
+            0.5037158727645874,
+            0.5327827334403992,
+            0.5412342548370361,
         ],
     }
 
@@ -69,11 +69,22 @@ def test_minimum_distance(client_query_result):
     )
 
 
-
-# def test_average_weighted_distance():
-#     grouped = emb.minimum_distance(
-#         client_query_result["metadatas"], client_query_result["distances"]
-#     )
-#     assert isinstance(grouped, dict)
-#     assert len(grouped.keys()) == 2
-#     assert all([k in grouped.keys() for k in ["metadatas", "distances"]])
+def test_average_weighted_distance(client_query_result):
+    grouped = emb.average_weighted_distance(
+        client_query_result["metadatas"], client_query_result["distances"]
+    )
+    assert isinstance(grouped, list)
+    assert len(grouped) == 6
+    titles = [
+        "402-polars.txt",
+        "012.txt",
+        "410-intersection-of-tabular-data-and-general-ai.txt",
+        "341-25-pandas-functions.txt",
+        "414-startup-row.txt",
+        "338-cibuildwheel-scikit-hep.txt",
+    ]
+    assert all([a[0] == b for a, b in zip(grouped, titles)])
+    npt.assert_allclose(
+        [a[1] for a in grouped],
+        [8.429576, 4.824124, 2.206777, 2.144225, 2.100645, 1.985246],
+    )
