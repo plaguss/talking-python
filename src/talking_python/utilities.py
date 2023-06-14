6e75e7f34ea1b22d9f14f21d68176a3f38dbab2e
agustin <agustin.piqueres@gmail.com>
2023-06-14 19:04:24 +0200

    feat: function to easily download chroma db

--- src/talking_python/utilities.py
+++ src/talking_python/utilities.py
@@ -12,3 +12,13 @@ def read_transcript(filename: Path) -> list[str]:
     """
     with open(filename, "r", encoding="utf-8") as f:
         return f.read().splitlines()
+
+
+def prepare_chroma() -> None:
+    """Function to download chroma database, used inside docker. """
+    import talking_python.release as rel
+    if not rel.get_chroma_dir().parent.exists():
+        rel.get_chroma_dir().parent.mkdir(parents=True)
+
+    rel.download_release_file(rel.get_release_url(), rel.get_chroma_dir().parent)
+    rel.untar_file(rel.get_chroma_dir().parent / "chroma.tar.gz")
